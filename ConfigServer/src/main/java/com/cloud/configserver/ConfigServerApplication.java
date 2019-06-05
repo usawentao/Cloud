5ba37266a2299d77a2d6ec0c1e2b9914335a6032
Edward <969819603@qq.com>
2019-06-05 10:42:44 +0800

    (update)为配置中心添加eureka注册配置

--- ConfigServer/src/main/java/com/cloud/configserver/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/cloud/configserver/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.cloud.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer //开启配置服务器的功能
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
