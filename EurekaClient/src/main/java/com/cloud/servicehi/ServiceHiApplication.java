fd0013bd93975830cc5cb701e2f1bfb02105c75a
Edward <969819603@qq.com>
2019-06-06 18:06:00 +0800

    (add)添加断路由视图功能

--- EurekaClient/src/main/java/com/cloud/servicehi/ServiceHiApplication.java
+++ EurekaClient/src/main/java/com/cloud/servicehi/ServiceHiApplication.java
@@ -1,15 +1,20 @@
 package com.cloud.servicehi;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 @RestController
 public class ServiceHiApplication {
 
@@ -21,8 +26,13 @@ public class ServiceHiApplication {
 	String port;
 
 	@RequestMapping("/hi")
+	@HystrixCommand(fallbackMethod = "hiError")
 	public String home(@RequestParam(value = "name",defaultValue = "Edward·Che") String name){
 		return "hi" + name + ",i am from port:" + port;
+	}
+
+	public String hiError(String name) {
+		return "hi,"+name+",sorry,error!";
 	}
 
 }
