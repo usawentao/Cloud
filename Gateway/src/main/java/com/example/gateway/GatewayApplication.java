9dead56565c505537629724e72aa0401b4e3210b
Edward <969819603@qq.com>
2019-08-30 14:46:27 +0800

    修整

--- Gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
@@ -19,7 +17,6 @@ public class GatewayApplication {
 
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		String httpUri = "http://httpbin.org:80";
 		//使用RouteLocatorBuilder的bean创建路由
 		return builder.routes()
 				.route(p -> p
@@ -28,22 +25,6 @@ public class GatewayApplication {
 					.filters(f -> f.addRequestHeader("Hello", "World"))
 					//将请求转发到以下url
 					.uri("http://httpbin.org:80"))
-				.route(p -> p
-					.path("/post")
-					.filters(f -> f.addRequestHeader("Style","post"))
-					.uri("http://httpbin.org:80"))
-				.route(p -> p
-					.host("*.histrix.com")
-					.filters(f->f
-						.hystrix(config -> config
-							.setName("mycmd")
-							.setFallbackUri("forward:/fallback")))
-					.uri(httpUri)
-				)
 				.build();
-	}
-	@RequestMapping("/falllback")
-	public Mono<String> fallback(){
-		return Mono.just("fullback")
 	}
 }
