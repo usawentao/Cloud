fb9f99eafdb603aef60762db586ffae40226d80e
Edward <969819603@qq.com>
2019-08-30 16:48:32 +0800

    predict限流

---
+++ Predicate/src/main/java/com/gateway/predicate/PredicateApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.predicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredicateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredicateApplication.class, args);
+	}
+
+}
